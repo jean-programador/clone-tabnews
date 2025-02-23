580c0871a580de0cd38f873414d347fac12c924a
jean-programador <jncorreia44@gmail.com>
2025-02-23 19:39:53 -0300

    adiciona prettier ao projeto, cria scripts `lint:check` e `lint:fix`, e ajusta estilização do código para padrão configurado no arquivo `.editorconfig`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Uma implentação do https://www.tabnews.com.br/ utilzado para estudos, realizados no https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 function Home() {
-    return (
+  return (
     <>
-        <center><h1>Eu te Amoo!!</h1></center>
-        <center><p>Mesmo se vôce fosse uma barata. hahaha</p></center>
+      <center>
+        <h1>Eu te Amoo!!</h1>
+      </center>
+      <center>
+        <p>Mesmo se vôce fosse uma barata. hahaha</p>
+      </center>
     </>
-    )
+  );
 }
 
 export default Home;
