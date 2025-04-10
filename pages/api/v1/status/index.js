493837183272771b07fbc543949c1cd47066fd7c
jean-programador <jncorreia44@gmail.com>
2025-04-10 16:57:49 -0300

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Corinthians Campeão Paulista 2025" });
+  const updatedAt = new Date().toISOString();
+
+  const versionResult = await database.query("SHOW server_version;");
+  const versionValue = versionResult.rows[0].server_version;
+
+  const maxConnectionResult = await database.query("SHOW max_connections;");
+  const maxConnectionValue = maxConnectionResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const openedConnectionValue = openedConnectionResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    database: {
+      dependencies: {
+        version: versionValue,
+        max_connections: Number(maxConnectionValue),
+        opened_connections: openedConnectionValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.database.dependencies.version).toBe("16.0");
+  expect(responseBody.database.dependencies.max_connections).toBeGreaterThan(0);
+  expect(responseBody.database.dependencies.opened_connections).toBe(1);
 });
