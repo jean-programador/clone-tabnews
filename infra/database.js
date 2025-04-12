aac7df84e19b1eafde5d7cfe378f7e43ad867864
jean-programador <jncorreia44@gmail.com>
2025-04-12 18:19:53 -0300

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV !== "development",
   });
 
   try {
@@ -15,6 +16,7 @@ async function query(queryObject) {
     return result;
   } catch (error) {
     console.error("Query failed", error);
+    throw error;
   } finally {
     await client.end();
   }
